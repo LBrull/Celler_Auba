3eaa695b7539d81d34fca87207a1d337d7c0fb97
Laura Brull Luelmo <laura.brull@est.fib.upc.edu>
2018-06-18 18:43:48 +0200

    proves  commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import com.mongodb.*;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Prueba conexión MongoDB");
+        MongoClient mongo = crearConexion();
+
+        if (mongo != null) {
+            System.out.println("Lista de bases de datos: ");
+
+            //printDatabases(mongo);
+
+        } else {
+            System.out.println("Error: Conexión no establecida");
+        }
     }
+
+    private static MongoClient crearConexion(){
+        MongoClient mongo = null;
+        mongo = new MongoClient("localhost", 27017);
+        return mongo;
+    }
+
+    private static void printDatabases(MongoClient mongo) {
+        List dbs = mongo.getDatabaseNames();
+
+        for (Object db : dbs) {
+            System.out.println(" - " + db.toString());
+        }
+    }
+
 }
