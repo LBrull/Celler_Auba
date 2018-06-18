4cf91f990fe00678f63c6f0210e055f18774940b
Laura Brull Luelmo <laura.brull@est.fib.upc.edu>
2018-06-18 20:13:46 +0200

    proves  commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,30 @@
 package com.company;
 
 import com.mongodb.*;
-import java.util.List;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class Main {
 
+    private static MongoClient mongo;
+    private static Document client1;
+
     public static void main(String[] args) {
 	System.out.println("Prueba conexión MongoDB");
-        MongoClient mongo = crearConexion();
+        mongo = crearConexion();
 
         if (mongo != null) {
             System.out.println("Lista de bases de datos: ");
+            MongoDatabase mongoDB = mongo.getDatabase("celler_aubarcaDB");
+            MongoCollection<Document> collection = mongoDB.getCollection("clients");
 
-            //printDatabases(mongo);
+            client1 = new Document();
+            client1.put("name", "Sivaraman");
+            client1.put("surname", "Tikitaka");
+            client1.put("telephone", 618526311);
+            client1.put("address", "C/Els Rosers, 23");
+            collection.insertOne(client1);
 
         } else {
             System.out.println("Error: Conexión no establecida");
@@ -25,11 +37,10 @@ public class Main {
         return mongo;
     }
 
-    private static void printDatabases(MongoClient mongo) {
-        List dbs = mongo.getDatabaseNames();
-
-        for (Object db : dbs) {
-            System.out.println(" - " + db.toString());
+    private static void printDatabase(MongoClient mongo) {
+        MongoDatabase db = mongo.getDatabase("celler_aubarcaDB");
+        MongoCollection<Document> collection = db.getCollection("celler_aubarcaDB");
+        for(int i=0; i<collection.count(); ++i) {
         }
     }
 
